f8733f6f6489e33626e356fd04ce01af362b18c9
tom <v_chenguanlin@baidu.com>
2021-10-05 17:25:35 +0800

    mq

--- workspace/mq/RabbitMq/rabbit-test/src/main/java/com/mq/rabbittest/fivemodel/Work.java
+++ workspace/mq/RabbitMq/rabbit-test/src/main/java/com/mq/rabbittest/fivemodel/Work.java
@@ -10,6 +10,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+
 /**
  * simple工作模式
  * 使用默认的Exchange和bind
@@ -24,6 +26,8 @@ public class Work {
     // 创建队列
     @Bean
     public Queue workQueue() {
+        LocalDate date1 = LocalDate.now();
+        LocalDate date2 = LocalDate.now();
         return new Queue("work_queue");
     }
 
